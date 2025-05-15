ced3a5a9c66394444ff0fc06fc3ef6354cb46829
Andrei Onel <onel@users.noreply.github.com>
2025-05-16 00:02:01 +0300

    Updating content for: AddressType.php

--- src/Form/AddressType.php
+++ src/Form/AddressType.php
@@ -13,6 +13,12 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class AddressType extends AbstractType
 {
+    /**
+     * Builds an address form with fields for personal information, address details, and contact information. The form includes fields for name, first name, last name, company (optional), postal address, postal code, city, country, phone number, and a submit button.
+     *
+     * @param FormBuilderInterface builder The form builder used to create the form
+     * @param array options An array of options for configuring the form
+     */
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
@@ -60,6 +66,13 @@ class AddressType extends AbstractType
         ;
     }
 
+    /**
+     * Configures the options for this form type by setting the data_class default to Address.
+     *
+     * @param OptionsResolver resolver The resolver for the options
+     * 
+     * @return void
+     */
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
